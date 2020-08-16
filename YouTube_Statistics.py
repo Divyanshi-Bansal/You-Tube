7a4fa145ef02913bedfa2dd396d9720ca5c14282
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-08-16 17:31:14 +0530

    now convert url into data

--- YouTube_Statistics.py
+++ YouTube_Statistics.py
@@ -1,5 +1,5 @@
-
-
+import requests
+import json
 
 class YTstats:
     def __init__(self,api_key,channel_id):
@@ -10,3 +10,5 @@ class YTstats:
     def get_channel_statistics(self):
         url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.channel_id}&key={self.api_key}'
         print(url)
+        json_url = requests.get(url)
+        data = json.load(json_url.text)
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from YouTube_Statistics import YTstats
 
+API_KEY = 'AIzaSyAU5h3nbVxmu5S6r84E5DA1X5i9lrWnsx0'
+channel_id = 'UCBR8-60-B28hp2BmDPdntcQ'
+
+yt = YTstats(API_KEY,channel_id)
+yt.get_channel_statistics()
